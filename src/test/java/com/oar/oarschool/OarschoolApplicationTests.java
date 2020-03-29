2414b86aa1d1bc13fac035f7221e02773e567694
ondrejsvojse <svojse.o@gmail.com>
2020-03-29 18:22:24 +0200

    Initial project dependencies

---
+++ src/test/java/com/oar/oarschool/OarschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oar.oarschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OarschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
